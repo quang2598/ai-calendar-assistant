555c50f541cfb5e54913a1539d6c4c64a8f73e86
Duong Tuan <tuan.duonghoang11@gmail.com>
2026-03-07 02:30:20 -0600

    feat: adding user avatar and email

--- apps/next.config.ts
+++ apps/next.config.ts
@@ -1,5 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "lh1.googleusercontent.com" },
+      { protocol: "https", hostname: "lh2.googleusercontent.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "lh4.googleusercontent.com" },
+      { protocol: "https", hostname: "lh5.googleusercontent.com" },
+      { protocol: "https", hostname: "lh6.googleusercontent.com" },
+    ],
+  },
+};
 
 export default nextConfig;
